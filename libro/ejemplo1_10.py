b6a55c9e3ec1bc5a74ef2e59fddcad999e574c19
sebvstianrodrigo <darkluna07@gmail.com>
2019-09-18 12:40:03 -0500

    Ejercicios del libro1

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,7 @@
+BASE = int(input("Dame la base del rectangulo:"))
+ALTU = int(input("Dame la altura del rectangulo:"))
+SUP = BASE * ALTU
+PER = 2*(BASE + ALTU)
+print(f"La superficie de tu rectangulo es { SUP } ")
+print(f"El perimetro de tu rectangulo es { PER } ")
+
---
+++ libro/ejemplo1_11.py
@@ -0,0 +1,9 @@
+# El programa dado un conjunto de datos de entrada invierte el orden de los mismos cuando los imprime
+A = int(input("Introduce el primer numero entero:"))
+B = int(input("Introduce el segundo numero entero:"))
+C = int(input("Introduce el tercer numero entero:"))
+D = int(input("Introduce el cuarto numero entero:"))
+print(D)
+print(C)
+print(B)
+print(A)
