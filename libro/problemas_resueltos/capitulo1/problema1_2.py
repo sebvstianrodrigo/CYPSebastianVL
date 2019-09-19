60a9508cf14a1b467d80eb9c56a533aeacfbea72
sebvstianrodrigo <darkluna07@gmail.com>
2019-09-19 17:35:33 -0500

    Problemas resueltos1

---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,4 @@
+BASE = float(input("Dame la base del triangulo:"))
+ALTU = float(input("Dame la altura del triangulo:"))
+SUP = (BASE*ALTU)/2
+print(f"El area del triangulo es {SUP} dado que la altura es {ALTU} U. y la base es {BASE} U.")
---
+++ libro/problemas_resueltos/capitulo1/problema1_3.py
@@ -0,0 +1,6 @@
+NOM = str(input("Escriba el nombre del dinosaurio:"))
+PES = float(input("Escriba el peso del dinosaurio en toneladas:"))
+LON = float(input("Escriba la longitud del dinosaurio en pies:"))
+PESKIL = PES * 1000
+LONMET = LON * 0.3047
+print(f"El nombre del dinosaurio es {NOM} El peso del dinosaurio en kilogramos es {PESKIL} kg. La longitud del dinosaurio en metros es {LONMET} m.") 
