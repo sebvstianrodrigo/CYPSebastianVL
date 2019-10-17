18e4e93d602aa56b26c6cd1134b6762a2bdc99d4
sebvstianrodrigo <darkluna07@gmail.com>
2019-10-16 19:09:33 -0500

    Se movieron de carpeta ejemplos

--- libro/problemas_resueltos/ejemplo2_2.py
+++
@@ -1,7 +0,0 @@
-SUE = float(input("Introduce el sueldo del trabajador:"))
-if SUE < 1000:
-    AUM = SUE * 0.15
-    NSUE = SUE + AUM
-    print(f"El nuevo sueldo del trabajador es ${NSUE}")
-else:
-    print("fin del programa")
--- libro/problemas_resueltos/ejemplo2_3.py
+++
@@ -1,5 +0,0 @@
-CAL = float(input("Introduzca la calificacion del alumno:"))
-if CAL >= 8:
-    print("El alumno esta aprobado")
-else:
-    print("El alumno esta reprobado")
