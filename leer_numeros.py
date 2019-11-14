b6525a37479a549ba7149e80c214a89415661792
sebvstianrodrigo <darkluna07@gmail.com>
2019-11-14 00:52:37 -0600

    Tarea 14 nov

--- leer_numeros.py
+++ leer_numeros.py
@@ -18,6 +18,15 @@ archivo.close()
 archivo = open("numeros.txt","rt")
 numeros2 = archivo.readlines()
 print(numeros2)
+lista_num2 = []
+for linea in numeros2:
+    for numero in linea.split(','):
+        lista_num2.append(int(numero))
+print(lista_num2)
+lista_num2.sort()
+print(f"\n Lista ordenada:{lista_num2} \n")
+
+print(f"El mayor es: {lista_num2[-1]} y el menor es: {lista_num2[0]}")
 archivo.close()
 
 archivo = open("numeros.txt","rt")
---
+++ letrero_de_colores.py
@@ -0,0 +1,3 @@
+print("\033[1;34m"+"Universidad Nacional Autonoma de Mexico")
+print("\033[1;33m"+"Facultad de Estudios Superiores Aragon")
+print("\033[1;32m"+"Sebastian Rodrigo Villa Luna")
