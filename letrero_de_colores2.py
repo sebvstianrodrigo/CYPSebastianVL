cd713a2e921b0ff5338cf3e58ae6610c8a2b1818
sebvstianrodrigo <darkluna07@gmail.com>
2019-11-14 14:59:08 -0600

    Tarea 14 nov

--- letrero_de_colores2.py
+++
@@ -1,5 +0,0 @@
-from terminal_text_color import TextColor
-tc = TextColor()
-print(tc.default_yellow("Universidad Nacional Autonoma de Mexico"))
-print(tc.bold_blue("Facultad de Estudios Superiores Aragon"))
-print(tc.bold_green("Sebastian Rodrigo Villa Luna"))
